82b3c6d67570eed23fbbeac5872c76829033ef76
Analyst1981 <analyst2011@qq.com>
2025-08-17 12:07:37 +0800

    Add src/gui/analysis_panel.py

---
+++ src/gui/analysis_panel.py
@@ -0,0 +1,12 @@
+"""
+智能分析面板
+显示AI分析结果和建议
+"""
+from PyQt6.QtWidgets import QWidget
+from loguru import logger
+
+class AnalysisPanel(QWidget):
+    def __init__(self):
+        super().__init__()
+    def perform_analysis(self):
+        pass
